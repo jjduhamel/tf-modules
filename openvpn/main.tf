de20cc6cc5ea75465a83589201c04cd80141bae4
John Duhamel <john.duhamel@gmail.com>
2017-05-06 21:29:30 -0700

    small fix

--- openvpn/main.tf
+++ openvpn/main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "openvpn" {
   ami = "${ var.ami_id }"
   instance_type = "t2.micro"
   key_name = "${ var.keypair }"
-  iam_instance_profile = "${ aws_iam_instance_profile.opevpn.id }"
+  iam_instance_profile = "${ aws_iam_instance_profile.openvpn.id }"
   subnet_id = "${ var.subnet_id }"
   vpc_security_group_ids = [ "${ aws_security_group.openvpn.id }" ]
   tags { Name = "OpenVPN" }
